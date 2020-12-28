8bfb8ee8352888022bd2097a160372e44a139c6a
Luis <a9301030@isec.pt>
2020-12-28 20:27:39 +0000

    Merge branch 'master' of https://github.com/HeitorRamosRosa/GPSKitchenDIY.git
    Conflicts:
    	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/pt/isec/supraindustries/kitchendiy/DataHandler.kt
+++ app/src/main/java/pt/isec/supraindustries/kitchendiy/DataHandler.kt
@@ -1,9 +1,17 @@
 package pt.isec.supraindustries.kitchendiy
 
+import android.content.Context
+import android.os.Environment
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_receita.*
+import java.io.*
+
 class DataHandler {
 
     companion object{
         var lista :  ArrayList<Int> = ArrayList()
+        var receitas_pt : ArrayList<Receita> = ArrayList()
+        var receitas_ing : ArrayList<Receita> = ArrayList()
 
         fun addInt(num: Int){
             lista.add(num)
@@ -18,5 +26,106 @@ class DataHandler {
         fun getSize() : Int{
             return lista.size
         }
+
+        fun LoadReceitas(mcontext: Context){
+            LoadReceitaByName(mcontext,"pao_com_chourico_pt", "pt")
+            LoadReceitaByName(mcontext,"esparguete_a_bolonhesa_pt", "pt")
+        }
+
+        fun LoadReceitaByName(mcontext : Context, fileName : String, lang: String){
+            val Context =  mcontext
+            val lang = lang
+            var string : String
+
+            //val IS : InputStream = Context.resources.openRawResource(R.raw.bolonhesa_de_lentilhas)
+            //o de baixo equivale ao de cima, mas encontrando o resource por string em vez de ir diretamente.
+            val IS : InputStream = Context.resources.openRawResource(Context.resources.getIdentifier(fileName,"raw",Context.packageName))
+
+            val reader = BufferedReader(InputStreamReader(IS))
+            do
+            {
+                var listaIngredientes: ArrayList<Ingrediente> = ArrayList()
+                var listaInstrucoes : ArrayList<String> = ArrayList()
+                var readMore = true
+                try
+                {
+                    var iterator = reader.lineSequence().iterator()
+                    while(iterator.hasNext()){
+                        string = iterator.next()
+                        System.out.println(string)
+                    }
+                    System.out.println("[EOF Reached]")
+
+                    val IS2 : InputStream = Context.resources.openRawResource(Context.resources.getIdentifier(fileName,"raw",Context.packageName))
+                    val reader2 = BufferedReader(InputStreamReader(IS2))
+                    iterator = reader2.lineSequence().iterator()
+
+                    while(iterator.hasNext()){
+                        string = iterator.next()
+                        if(string.equals("Ingredientes:")){
+                            System.out.println("Extracting ingredientes.")
+                            break;
+                        }
+                    }
+                    while(iterator.hasNext() && readMore ){
+                        string = iterator.next()
+                        if(string.equals("Preparação:")){
+                            readMore = false
+                            System.out.println("Extracting Done.")
+                            break;
+                        }
+                        var quantidade = iterator.next()
+                        //System.out.println("Extracting: ["+string+"] q:["+quantidade+"]")
+                        var temp : Ingrediente = Ingrediente(string, quantidade)
+                        listaIngredientes.add(temp)
+                    }
+
+                    while(iterator.hasNext()){
+                        string = iterator.next()
+                        if(string.equals("Preparação:"))
+                            System.out.println("Getting instruções");
+                        else
+                        {
+                            listaInstrucoes.add(string)
+                        }
+                    }
+                    System.out.println("Got "+listaIngredientes.size+" ingredientes")
+
+                    System.out.println("Creating and adding receita to receitaList")
+
+                    var tempReceita = Receita(fileName,listaIngredientes, listaInstrucoes)
+                    if(lang.equals("pt"))
+                        receitas_pt.add(tempReceita)
+                    else if(lang.equals("ing"))
+                        receitas_ing.add(tempReceita)
+                    break;
+                }catch(e: IOException)
+                {
+                    e.printStackTrace()
+                }
+            }while(true)
+        }
+
+        /*
+       fun ler() {
+            var string: String? = ""
+            val stringBuilder = StringBuilder()
+            val `is`: InputStream = this.resources.openRawResource(R.raw.bolonhesa_de_lentilhas)
+            val reader = BufferedReader(InputStreamReader(`is`))
+            while (true) {
+                try {
+                    if (reader.readLine().also { string = it } == null) break
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+                stringBuilder.append(string).append("\n")
+                tv_receita.text = stringBuilder
+            }
+            `is`.close()
+        }
+        */
+
+
     }
+
 }
--- app/src/main/java/pt/isec/supraindustries/kitchendiy/MainActivity.kt
+++ app/src/main/java/pt/isec/supraindustries/kitchendiy/MainActivity.kt
@@ -3,13 +3,80 @@ package pt.isec.supraindustries.kitchendiy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import java.util.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+    lateinit var spinner: Spinner
+    lateinit var locale: Locale
+    private var currentLanguage = "pt"
+    private var currentLang: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("Debug","Doing load receitas")
+        DataHandler.LoadReceitas(applicationContext, )
+        Log.i("Debug","Load Receitas Done")
+        System.out.println("In main: ")
+        System.out.println(DataHandler.receitas_pt[0].getInfo())
+        System.out.println(DataHandler.receitas_pt[1].getInfo())
+        currentLanguage = intent.getStringExtra(currentLang).toString()
+        spinner = findViewById(R.id.spinner)
+        val list = ArrayList<String>()
+        list.add("Select Language")
+        list.add("Português")
+        list.add("English")
+
+        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, list)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                when (position) {
+                    0 -> {
+                    }
+                    1 -> setLocale("pt")
+                    2 -> setLocale("en")
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+    }
+    private fun setLocale(localeName: String) {
+        if (localeName != currentLanguage) {
+            locale = Locale(localeName)
+            val res = resources
+            val dm = res.displayMetrics
+            val conf = res.configuration
+            conf.locale = locale
+            res.updateConfiguration(conf, dm)
+            val refresh = Intent(
+                    this,
+                    MainActivity::class.java
+            )
+            refresh.putExtra(currentLang, localeName)
+            startActivity(refresh)
+        } else {
+            Toast.makeText(
+                    this@MainActivity, "Language, , already, , selected)!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    override fun onBackPressed() {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        finish()
+        exitProcess(0)
     }
     fun loadEscolherIngredientes(view: View) {
         val intent = Intent(this, EscolherIngredientes::class.java)
